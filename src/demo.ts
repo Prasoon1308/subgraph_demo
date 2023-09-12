7c0f3345b97cd267c199e58f185a80ac544144cf
Prasoon <prasoon1308@gmail.com>
2023-09-12 09:46:38 +0530

    graph build

--- src/demo.ts
+++ src/demo.ts
@@ -1,15 +1,28 @@
-import { value as valueEvent } from "../generated/demo/demo"
-import { value } from "../generated/schema"
+import { value as valueEvent } from "../generated/demo/demo";
+import { valuetwo as valuetwoEvent } from "../generated/demo/demo";
+import { value, valuetwo } from "../generated/schema";
 
 export function handlevalue(event: valueEvent): void {
   let entity = new value(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.one = event.params.one
+  );
+  entity.one = event.params.one;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+}
+export function handlevaluetwo(event: valuetwoEvent): void {
+  let entity = new valuetwo(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.two = event.params.two;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
